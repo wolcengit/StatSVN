85e3422660124041c5925fdd77dc7fedced4c616
Wolcen <wolcen@msn.com>
2021-12-26 17:15:02 +0800

    fix compare version

--- statsvn/src/net/sf/statsvn/util/SvnStartupUtils.java
+++ statsvn/src/net/sf/statsvn/util/SvnStartupUtils.java
@@ -62,7 +62,7 @@ public class SvnStartupUtils implements ISvnVersionProcessor {
 						final String versionString = line.substring(m.start(), m.end());
 
 						// we perform a simple string comparison against the version numbers
-						if (versionString.compareTo(SVN_MINIMUM_VERSION) >= 0) {
+						if (compareVersion(versionString,SVN_MINIMUM_VERSION) >=0) {
 							return versionString; // success
 						} else {
 							throw new SvnVersionMismatchException(versionString, SVN_MINIMUM_VERSION);
@@ -96,6 +96,48 @@ public class SvnStartupUtils implements ISvnVersionProcessor {
      */
 	public synchronized boolean checkDiffPerRevPossible(final String version) {
 		// we perform a simple string comparison against the version numbers
-		return version.compareTo(SVN_MINIMUM_VERSION_DIFF_PER_REV) >= 0;
+		return compareVersion(version,SVN_MINIMUM_VERSION_DIFF_PER_REV) >= 0;
 	}
+
+   /**
+     * 比较版本大小
+     *
+     * 说明：支n位基础版本号+1位子版本号
+     * 示例：1.0.2>1.0.1 , 1.0.1.1>1.0.1
+     *
+     * @param version1 版本1
+     * @param version2 版本2
+     * @return 0:相同 1:version1大于version2 -1:version1小于version2
+     */
+    public int compareVersion(String version1, String version2) {
+        if (version1.equals(version2)) {
+            return 0; //版本相同
+        }
+        String[] v1Array = version1.split("\\.");
+        String[] v2Array = version2.split("\\.");
+        int v1Len = v1Array.length;
+        int v2Len = v2Array.length;
+        int baseLen = 0; //基础版本号位数（取长度小的）
+        if(v1Len > v2Len){
+            baseLen = v2Len;
+        }else{
+            baseLen = v1Len;
+        }
+
+        for(int i=0;i<baseLen;i++){ //基础版本号比较
+            if(v1Array[i].equals(v2Array[i])){ //同位版本号相同
+                continue; //比较下一位
+            }else{
+                return Integer.parseInt(v1Array[i])>Integer.parseInt(v2Array[i]) ? 1 : -1;
+            }
+        }
+        //基础版本相同，再比较子版本号
+        if(v1Len != v2Len){
+            return v1Len > v2Len ? 1:-1;
+        }else {
+            //基础版本相同，无子版本号
+            return 0;
+        }
+    }
+
 }
